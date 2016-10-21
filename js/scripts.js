49f50dc05eb55ec4546b191ad4271f68c999808b
Chris Clifford <cclifford82@gmail.com>
2016-10-21 14:46:22 -0700

    add front end code that adds a link to the learn more button

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-// Business logic:
+// ****** Business logic ******
 
+/* The following function returns a track suggestion string
+based on answers from a form
+*/
 var whichTrack = function(answer1, answer2, answer3, answer4, answer5, answer6) {
   if ((answer1 === "option1" && answer2 === "option1") || (answer1 === "option1" && answer2 === "option2") || (answer1 === "option3" && answer2 === "option1")) {
     return "CSS and Design";
@@ -12,11 +15,15 @@ var whichTrack = function(answer1, answer2, answer3, answer4, answer5, answer6) 
   } else if ((answer1 === "option2" && answer6 === "option1")) {
     return "PHP and Drupal"
   } else {
-    return "no preference"
+    return "no suggestion"
   }
 
 };
 
+
+/* The following function returns a link string
+based on the suggestion returned from whichTrack()
+*/
 var whichLink = function(track) {
   if (track === "CSS and Design") {
     return "http://www.epicodus.com/design"
@@ -33,12 +40,14 @@ var whichLink = function(track) {
 
 
 
-// User interface logic:
+
+// ***** User interface logic *****
 
 $(document).ready(function() {
   $("form#track-qestions").submit(function() {
     event.preventDefault();
 
+    //get the answers from the radio buttons on form
     var answer1 = $("input:radio[name=question1]:checked").val();
     var answer2 = $("input:radio[name=question2]:checked").val();
     var answer3 = $("input:radio[name=question3]:checked").val();
@@ -46,15 +55,25 @@ $(document).ready(function() {
     var answer5 = $("input:radio[name=question5]:checked").val();
     var answer6 = $("input:radio[name=question6]:checked").val();
 
-
+    //call function to get track suggestion and store in result
     var result = whichTrack(answer1, answer2, answer3, answer4, answer5, answer6);
 
+    //get link to display to user
+    var link = whichLink(result);
+
+    //hide questions
     $(".question-form").toggle();
-    if (result === "no preference") {
-      $(".no-track").fadeToggle();
+    $("#nav-q").removeClass("active");
+    $("#nav-track").addClass("active");
+
+    //display results based on suggestion
+    if (result === "no suggestion") {
+      $(".no-track").fadeToggle();   //show info if no suggestion returned
     } else {
-      $("#answer").text(result);
-      $(".track-suggestion").fadeToggle();
+
+      $("#answer").text(result);  //insert track suggestion into span element #answer
+      $("#learn-more").attr("href", link)
+      $(".track-suggestion").fadeToggle(); //show suggestion div
 
     }
 
